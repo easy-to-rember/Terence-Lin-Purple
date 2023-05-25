ff8539377f4cb3f357f05433bc0866c024f8cd32
CELO <cleotran05@gmail.com>
2023-05-24 20:00:09 -0400

    dropping bombs part 3 step 18

--- Dropping Bombs/Assets/Scripts/RocketAnimation.cs
+++ Dropping Bombs/Assets/Scripts/RocketAnimation.cs
@@ -9,12 +9,13 @@ public class RocketAnimation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        animator.SetFloat("HAxis", horizontal);
     }
 }
